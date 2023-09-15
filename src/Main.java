db37c9d0cc35de87015f42424647c814927ab15d
luong.an <luong.an@outlook.com>
2023-09-15 11:46:50 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,5 @@
+/* Object : lifecyle from creation to destruction, can have relationship with each other
+as inheritance. Contain both data and functionality
+Classes:  Object is an instance of class. A data type.
+n
+ */
